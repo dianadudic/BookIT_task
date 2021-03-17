61cb425fca68e35823b5a531700c7aec68a8fd9a
dianadudic <dianadudic@gmail.com>
2021-03-17 11:22:56 -0400

    - verifying rooms on dashboard #2

---
+++ src/test/java/com/Step_Definitions/Dashboard_StepDefinitions.java
@@ -0,0 +1,30 @@
+package com.Step_Definitions;
+
+import com.Pages.DashBoardPage;
+import com.Pages.LoginPage;
+import com.utility.BrowserUtils;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Dashboard_StepDefinitions {
+   LoginPage loginPage = new LoginPage();
+   DashBoardPage dashBoardPage = new DashBoardPage();
+
+    @Given("User is on dashboard")
+    public void user_is_on_dashboard() {
+        loginPage.logInToBookIt();
+
+        BrowserUtils.wait(5);
+    }
+    @Then("User can see all rooms")
+    public void user_can_see_all_rooms(List<String> rooms) {
+
+        for (WebElement eachRoom: dashBoardPage.Rooms){
+            System.out.println("eachRoom = " + eachRoom.getText());
+        }
+
+    }
+}
